da61e9792814dda35970972a87df31b190078d47
mrs-eload <elodie.philippe90@gmail.com>
2021-01-14 23:52:32 +0100

    fix minimize

--- webpack.config.js
+++ webpack.config.js
@@ -206,7 +206,7 @@ const getConfig = (support, isProd) => {
             })
         );
         config.plugins.push(
-            new webpack.optimize.UglifyJsPlugin(
+            new webpack.optimize.minimize(
                 {
                     compress: {
                         warnings: false,
